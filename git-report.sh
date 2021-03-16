4fd707d2eb8ce2405c86813ef36d0eeefc017766
Vitthi <50586827+Vitthi@users.noreply.github.com>
2021-03-16 17:23:51 +0530

    removed pretty

--- git-report.sh
+++ git-report.sh
@@ -30,7 +30,7 @@ generate_git_log() {
 	#--reverse	Output the commits chosen to be shown in reverse order
 	#--name-status	Show only names and status of changed files	
 	
-	git log --pretty=format:%Cblue%ad%Creset %aN %Cgreen%s%Creset --date=local --name-status> $TEMP_FILE
+	git log --date=local --name-status> $TEMP_FILE
 	
 	# replace tab to ;
 	cat $TEMP_FILE | tr "	" ";" > $GIT_LOG_FILE
