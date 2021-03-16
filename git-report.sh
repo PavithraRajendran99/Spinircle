f03b208fe48a0807b3732adb703ea4429d806f08
Vitthi <50586827+Vitthi@users.noreply.github.com>
2021-03-16 17:32:27 +0530

    pretty check-2

--- git-report.sh
+++ git-report.sh
@@ -30,7 +30,7 @@ generate_git_log() {
 	#--reverse	Output the commits chosen to be shown in reverse order
 	#--name-status	Show only names and status of changed files	
 	
-	git log --date=local --name-status> $TEMP_FILE
+	git log --pretty=format:"%an , %ad : %s" --date=local --name-status> $TEMP_FILE
 	
 	# replace tab to ;
 	cat $TEMP_FILE | tr "	" ";" > $GIT_LOG_FILE
