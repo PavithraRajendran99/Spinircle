2b2016abc940f6071dab02585281068bbe342124
Vitthi <50586827+Vitthi@users.noreply.github.com>
2021-03-17 20:49:10 +0530

    pull request inside shell check-1

--- git-report.sh
+++ git-report.sh
@@ -31,7 +31,7 @@ generate_git_log() {
 	#--name-status	Show only names and status of changed files	
 	
 	git log --pretty=format:"%an , %ad : %s" --date=local --name-status>$TEMP_FILE
-	
+	git pull-request
 	# replace tab to ;
 	##cat $TEMP_FILE | tr "	" ";" >$GIT_LOG_FILE 
 }
